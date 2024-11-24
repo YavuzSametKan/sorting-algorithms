c725d0dea3843d2bab76fe74bc3ca99511577ff1
yavuz samet kan <yssk.personal@gmail.com>
2024-11-24 17:10:07 +0300

    Demo File Updated

--- src/SortingDemo/Program.cs
+++ src/SortingDemo/Program.cs
@@ -1,34 +1,86 @@
-﻿using Utils.ArrayUtils;
+﻿using System.Diagnostics;
+using Utils.ArrayUtils;
 using Sorting;
 
 class Program{
 
-    public static void InsertionSortDemo(){
-        // Generate array for insertion sort
-        var arr = ArrayUtils.GenerateArr(arrSize: 8, low: 0, high: 100);
+    public static void InsertionSortDemo(int[]? arr = null, bool printing = true){
+        // If there is no array, generate a random array
+        if (arr == null) 
+            arr = ArrayUtils.GenerateArr(10, low: 0, high: 100);
+        
+        // Calculating to working time
+        Stopwatch stopwatch = Stopwatch.StartNew();
         
         // Sorting
         InsertionSort.Sort(arr);
-
-        // Printing to console
-        Console.WriteLine("Insertion Sort:");
-        ArrayUtils.PrintArr(arr);
+        
+        // Calculating is finish
+        stopwatch.Stop();
+        
+        // Working time
+        Console.WriteLine($"Insertion Sort: {stopwatch.Elapsed} time");
+        
+        if (printing) // Printing array to console
+            ArrayUtils.PrintArr(arr);
     }
     
-    public static void MergeSortDemo(){
-        // Generate array for insertion sort
-        var arr = ArrayUtils.GenerateArr(arrSize: 8, low: 0, high: 100);
+    public static void MergeSortDemo(int[]? arr = null, bool printing = true){
+        // If there is no array, generate a random array
+        if (arr == null) 
+            arr = ArrayUtils.GenerateArr(10, low: 0, high: 100);
+        
+        // Calculating to working time
+        Stopwatch stopwatch = Stopwatch.StartNew();
         
         // Sorting
         MergeSort.Sort(arr);
+        
+        // Calculating is finish
+        stopwatch.Stop();
+        
+        // Working time
+        Console.WriteLine($"Merge Sort: {stopwatch.Elapsed} time");
+        
+        if (printing) // Printing array to console
+            ArrayUtils.PrintArr(arr);
+    }
+    
+    public static void QuickSortDemo(int[]? arr = null, bool printing = true){
+        // If there is no array, generate a random array
+        if (arr == null) 
+            arr = ArrayUtils.GenerateArr(10, low: 0, high: 100);
+        
+        // Calculating to working time
+        Stopwatch stopwatch = Stopwatch.StartNew();
+        
+        // Sorting
+        QuickSort.Sort(arr);
+        
+        // Calculating is finish
+        stopwatch.Stop();
 
-        // Printing to console
-        Console.WriteLine("Merge Sort:");
-        ArrayUtils.PrintArr(arr);
+        // Working time
+        Console.WriteLine($"Quick Sort: {stopwatch.Elapsed} time");
+        
+        if (printing) // Printing array to console
+            ArrayUtils.PrintArr(arr);
     }
 
-    public static void Main(string[] args){
-        InsertionSortDemo();
-        MergeSortDemo();
+
+    public static void Main(string[] args)
+    {
+        int[] arr = ArrayUtils.GenerateArr(
+            arrSize: 100000,
+            low: 0,
+            high: 9999999
+        );
+        
+        Array.Sort(arr);
+        Array.Reverse(arr);
+        
+        InsertionSortDemo(arr, printing: false);
+        MergeSortDemo(arr, printing: false);
+        QuickSortDemo(arr, printing: false);
     }
 }
